7b6c359b5f4a4c98d90ec5ea2526ee1684568da9
Gregory Izatt <gregory.izatt@gmail.com>
2019-01-21 16:12:09 -0500

    Refactoring environment generator

--- data/planar_scene_arrangement_utils.py
+++ data/planar_scene_arrangement_utils.py
@@ -24,9 +24,6 @@ from pydrake.all import (
 )
 from pydrake.multibody.rigid_body import RigidBody
 from underactuated import PlanarRigidBodyVisualizer
-from planar_scene_arrangement_utils import (
-    add_cube, build_rbt_from_summary, draw_board_state
-)
 
 '''
 
@@ -35,6 +32,24 @@ shapes. Generates a uniform distribution over
 feasible configurations.
 
 '''
+
+
+def add_cube(rbt, name, size, frame, color):
+    link = RigidBody()
+    link.set_name(name)
+    link.add_joint(
+        rbt.world(), RollPitchYawFloatingJoint(
+            name + "_base", frame.get_transform_to_body()))
+    box_element = Box(size)
+    visual_element = VisualElement(box_element, np.eye(4), color)
+    link.AddVisualElement(visual_element)
+    link.set_spatial_inertia(np.eye(6))
+    rbt.add_rigid_body(link)
+
+    collision_element = CollisionElement(box_element, np.eye(4))
+    collision_element.set_body(link)
+    rbt.addCollisionElement(collision_element, link, "default")
+
 
 # Factories for each object. Edit this list to
 # change what objects might be spawned.
@@ -176,6 +191,41 @@ def sample_scene_anisotropic_uniform_random(p=0.5, num_objects=None):
     q0 = np.zeros(rbt.get_num_positions())
 
     return rbt, q0, rbt_summary
+
+
+def build_rbt_from_summary(rbt_summary):
+    rbt = RigidBodyTree()
+    AddFlatTerrainToWorld(rbt)
+
+    num_objects = int(rbt_summary["n_objects"])
+    q0 = np.zeros(6*num_objects)
+    for i in range(num_objects):
+        obj = rbt_summary["obj_%04d" % i]
+        class_name = obj["class"]
+        full_name = "%s_%03d" % (class_name, i)
+        pose = obj["pose"]
+        object_init_frame = RigidBodyFrame(
+            "%s_init_frame" % full_name, rbt.world(),
+            [0., 0., 0.5],
+            [0., 0., 0.0])
+        q0[(i*6):(i*6+6)] = np.array([
+            pose[0], pose[1], 0.0, 0., 0., pose[2]])
+        object_adders[class_name](rbt, full_name, object_init_frame)
+
+    rbt.compile()
+
+    return rbt, q0
+
+
+def draw_board_state(ax, rbt, q):
+    Tview = np.array([[1., 0., 0., 0.],
+                      [0., 1., 0., 0.],
+                      [0., 0., 0., 0.],
+                      [0., 0., 0., 1.]])
+
+    viz = PlanarRigidBodyVisualizer(
+        rbt, Tview, xlim=[-0.25, 1.25], ylim=[-0.25, 1.25], ax=ax)
+    viz.draw(q)
 
 
 def main(stdscr, args):
---
+++ data/single_planar_box_arrangements/normal_random/fixed_2_objects/README.md
@@ -0,0 +1,3 @@
+mu=np.array([0.5, 0.5, np.pi]),
+sigma=np.array([0.1, 0.1, np.pi/2.])
+        
