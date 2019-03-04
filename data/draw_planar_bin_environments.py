6d5b7c8f5312679becdfee0229a8a94e16a608fc
Gregory Izatt <gregory.izatt@gmail.com>
2019-03-04 17:02:40 -0500

    Draw planar bin env by name

--- data/draw_planar_bin_environments.py
+++ data/draw_planar_bin_environments.py
@@ -13,9 +13,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset_path",
                         help="Planar bin dataset to visualize.")
-    parser.add_argument("-n", "--index",
+    parser.add_argument("-i", "--index",
                         type=int,
                         default=-1,
+                        help="Entry to draw, or -1 for random.")
+    parser.add_argument("-n", "--name",
+                        type=str,
+                        default="",
                         help="Entry to draw, or -1 for random.")
     args = parser.parse_args()
 
@@ -23,14 +27,22 @@ if __name__ == "__main__":
     dataset = dataset_utils.ScenesDataset(data_file)
 
     plt.plot(10, 10)
-    if args.index < 0:
+    if args.index < 0 and len(args.name) == 0:
         while (1):
             dataset_utils.DrawYamlEnvironmentPlanar(
                 dataset[np.random.randint(0, len(dataset))],
                 "planar_bin", ax=plt.gca())
             plt.pause(1.0)
             plt.gca().clear()
-    else:
+    elif args.index >= 0:
         dataset_utils.DrawYamlEnvironmentPlanar(
             dataset[args.index], "planar_bin", ax=plt.gca())
+        print dataset[args.index]
+        plt.show()
+    else:
+        print dataset.get_environment_index_by_name(args.name)
+        dataset_utils.DrawYamlEnvironmentPlanar(
+            dataset[dataset.get_environment_index_by_name(args.name)],
+            "planar_bin", ax=plt.gca())
+        print dataset[args.index]
         plt.show()
