814ceade48b2d566bfccf5d5c46021acc567cf61
Gregory Izatt <gregory.izatt@gmail.com>
2019-03-11 19:10:06 -0400

    Split into subfiles / train / test

--- data/planar_bin/split_into_subfiles.py
+++ data/planar_bin/split_into_subfiles.py
@@ -1,28 +1,64 @@
 import numpy as np
+import random
+import os
 import yaml
 try:
     from yaml import CLoader as Loader
 except ImportError:
-    print "WARNING: Defaulting to non-C [slow] YAML loader."
+    print("WARNING: Defaulting to non-C [slow] YAML loader.")
     from yaml import Loader
 
-f = open("planar_bin_static_scenes_stacks.yaml", "r")
-fo = open("planar_bin_static_scenes_stacks_new.yaml", "w")
+f_in_base = "planar_bin_static_scenes_geometric"
+train_fraction = 0.8
+max_environments_per_subfile = 100
 
-raw_yaml_environments = yaml.load(f, Loader=Loader)
-print "Starting with ", len(raw_yaml_environments), " environments."
-prune_keys = []
-for key in raw_yaml_environments:
-    env = raw_yaml_environments[key]
-    for k in range(env["n_objects"]):
-        obj_yaml = env["obj_%04d" % k]
-        # Check if x or z is outside of bounds
-        pose = np.array(obj_yaml["pose"])
-        if pose[0] > 2.0 or pose[0] < -2.0 or pose[1] > 1.0 or pose[1] < 0.0:
-            prune_keys.append(key)
-            break
+with open(f_in_base + ".yaml", "r") as f:
+    raw_yaml_environments = yaml.load(f, Loader=Loader)
+num_envs = len(raw_yaml_environments)
+print("Input: %d environments" % num_envs)
 
-for key in prune_keys:
-    del raw_yaml_environments[key]
-print "Ending with ", len(raw_yaml_environments), " environments."
-yaml.dump(raw_yaml_environments, fo)
+all_keys = raw_yaml_environments.keys()
+inds = range(num_envs)
+random.shuffle(inds)
+
+end_train_ind = int(train_fraction * num_envs)
+start_test_ind = end_train_ind + 1
+
+print("Splitting into %d train, %d test in files of size %d" % (
+    int(end_train_ind),
+    int(num_envs - end_train_ind),
+    max_environments_per_subfile))
+
+
+# Chunk off training envs
+folder_name = f_in_base + "_train"
+os.mkdir(folder_name)
+for start_ind in range(0, end_train_ind, max_environments_per_subfile):
+    end_ind = min(start_ind + max_environments_per_subfile, end_train_ind)
+
+    these_envs = {all_keys[ind]: raw_yaml_environments[all_keys[ind]]
+                  for ind in inds[start_ind:end_ind]}
+
+    fname = os.path.join(
+        folder_name,
+        f_in_base + "_train_%04d_to_%04d.yaml" % (start_ind, end_ind))
+    with open(fname, "w") as f:
+        print("Saving %d envs to file %s" % (len(these_envs), fname))
+        yaml.dump(these_envs, f)
+
+# Chunk off test envs
+folder_name = f_in_base + "_test"
+os.mkdir(folder_name)
+for start_ind in range(start_test_ind, num_envs, max_environments_per_subfile):
+    end_ind = min(start_ind + max_environments_per_subfile, num_envs)
+
+    these_envs = {all_keys[ind]: raw_yaml_environments[all_keys[ind]]
+                  for ind in inds[start_ind:end_ind]}
+
+    fname = os.path.join(
+        folder_name,
+        f_in_base + "_test_%05d_to_%05d.yaml" % (
+            start_ind-start_test_ind, end_ind-start_test_ind))
+    with open(fname, "w") as f:
+        print("Saving %d envs to file %s" % (len(these_envs), fname))
+        yaml.dump(these_envs, f)
