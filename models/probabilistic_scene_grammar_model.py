c8f7591bb650dc2af4e74d691347814f1cc24d8c
Gregory Izatt <gregory.izatt@gmail.com>
2019-06-19 14:17:50 -0400

    And/or/set rules

--- models/probabilistic_scene_grammar_model.py
+++ models/probabilistic_scene_grammar_model.py
@@ -5,78 +5,119 @@ import numpy as np
 import time
 
 import torch
+import torch.distributions.constraints as constraints
 import pyro
 import pyro.distributions as dist
 from pyro import poutine
 
-
-class ProductionRule(object):
-    def __init__(self):
-        raise NotImplementedError()
-    def __call__(self, *args):
-        return [x(*args) for x in self.products]
-
-class AndRule(ProductionRule):
-    def __init__(self, *args):
-        self.products = args
-
-
-class Node(object):
+class OrNode(object):
     def __init__(self, production_rules, production_weights):
         if len(production_weights) != len(production_rules):
             raise ValueError("# of production rules and weights must match.")
-
+        if len(production_weights) == 0:
+            raise ValueError("Must have nonzero number of production rules.")
         self.production_rules = production_rules
-        if len(self.production_rules) > 0:
-            self.production_dist = dist.Categorical(production_weights)
-        else:
-            self.production_dist = None
+        self.production_dist = dist.Categorical(production_weights)
 
-        self.attributes = []
+    def sample_production_rule(self, site_prefix, obs=None):
+        sampled_rule = pyro.sample(site_prefix + "_or_sample", self.production_dist, obs=obs)
+        return self.production_rules[sampled_rule](site_prefix)
 
-    def sample_production_rule(self, site_name, obs=None):
-        if not self.production_dist:
-            raise ValueError("Trying to sample from a terminal node.")
-        sampled_rule = pyro.sample(site_name, self.production_dist, obs=obs)
-        return self.production_rules[sampled_rule](*self.attributes)
 
-class TerminalNode(Node):
+class AndNode(object):
+    def __init__(self, production_rules):
+        if len(production_rules) == 0:
+            raise ValueError("Must have nonzero # of production rules.")
+        self.production_rules = production_rules
+        
+    def sample_production_rule(self, site_prefix):
+        return [x(site_prefix) for x in self.production_rules]
+
+
+class ExhaustiveSetNode(object):
+    def __init__(self, site_prefix, production_rules):
+        # Make a categorical distribution over
+        # every possible combination of production rules
+        # that could be active.
+        num_combinations = 2**len(production_rules)
+        self.exhaustive_set_weights = pyro.param(
+            site_prefix + "_exhaustive_set_weights",
+            torch.ones(num_combinations) / num_combinations,
+            constraint=constraints.simplex)
+
+        self.production_dist = dist.Categorical(self.exhaustive_set_weights)
+        self.site_prefix = ""
+        self.production_rules = production_rules
+
+    def sample_production_rule(self, site_prefix):
+        selected_rules = pyro.sample(
+            site_prefix + "_exhaustive_set_sample",
+            self.production_dist)
+        # Select out the appropriate rules
+        output = []
+        for k, rule in enumerate(self.production_rules):
+            if (selected_rules >> k) & 1:
+                output += rule(site_prefix)
+        return output
+
+
+class TerminalNode(object):
     def __init__(self):
         Node.__init__(self, [], [])
 
-class PlaceSetting(Node):
-    def __init__(self):
-        production_rules = [ AndRule(Dish), AndRule(Dish, Cup) ]
-        production_weights = torch.tensor([0.2, 0.8])
-        Node.__init__(self, production_rules, production_weights)
+
+class PlaceSetting(ExhaustiveSetNode):
+    def __init__(self, pose):
+        production_rules = [ self._produce_place_setting,
+                             self._produce_dish_and_cup ]
+        ExhaustiveSetNode.__init__(self, "place_setting", production_rules)
+        self.pose = pose
+
+    def _produce_place_setting(self, site_prefix):
+        return [Dish(self.pose)]
+
+    def _produce_dish_and_cup(self, site_prefix):
+        return [Dish(self.pose), Cup(self.pose + torch.tensor([0., 0.1, 0.]))]
+
 
 class Dish(TerminalNode):
-    pass
+    def __init__(self, pose):
+        self.pose = pose
 
 class Cup(TerminalNode):
-    pass
+    def __init__(self, pose):
+        self.pose = pose
 
-class RootNode(Node):
+class RootNode(OrNode):
     def __init__(self):
-        production_rules = [ AndRule(PlaceSetting), AndRule(PlaceSetting, PlaceSetting) ]
+        production_rules = [ self._produce_one_place_setting,
+                             self._produce_two_place_settings]
         production_weights = torch.tensor([ 0.75, 0.25 ])
-        Node.__init__(self, production_rules, production_weights)
+        OrNode.__init__(self, production_rules, production_weights)
+
+    def _produce_one_place_setting(self, site_prefix):
+        return [PlaceSetting(pose=torch.tensor([0., 0., 0.]))]
+
+    def _produce_two_place_settings(self, site_prefix):
+        return [PlaceSetting(pose=torch.tensor([0., 0., 0.])),
+                PlaceSetting(pose=torch.tensor([1., 0., 0.]))]
+
 
 class ProbabilisticSceneGrammarModel():
-
-
     def __init__(self):
         pass
 
     def model(self, data=None):
         nodes = [RootNode()]
+        all_terminal_nodes = []
         num_productions = 0
         iter_k = 0
         while len(nodes) > 0:
             node = nodes.pop(0)
             if isinstance(node, TerminalNode):
                 # Instantiate
-                print("Instantiating terminal node: ", node)
+                print("Instantiating terminal node: ", node, " at pose ", node.pose)
+                all_terminal_nodes.append(node)
             else:
                 print("Expanding non-terminal node ", node)
                 # Expand by picking a production rule
@@ -85,7 +126,7 @@ class ProbabilisticSceneGrammarModel():
                 num_productions += 1
             iter_k += 1
 
-        return pyro.sample("test", dist.Normal(0., 1.))
+        return all_terminal_nodes
 
     def guide(self, data):
         pass
