edffb6c26b1faf91569bb5ac652a446092050ad8
Gregory Izatt <gregory.izatt@gmail.com>
2019-06-19 13:40:28 -0400

    Starting to get flow of nodes and production rules going

--- models/probabilistic_scene_grammar_model.py
+++ models/probabilistic_scene_grammar_model.py
@@ -1,30 +1,89 @@
+from __future__ import print_function
+
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+import torch
 import pyro
 import pyro.distributions as dist
 from pyro import poutine
 
 
+class ProductionRule(object):
+    def __init__(self):
+        raise NotImplementedError()
+    def __call__(self, *args):
+        return [x(*args) for x in self.products]
+
+class AndRule(ProductionRule):
+    def __init__(self, *args):
+        self.products = args
+
+
+class Node(object):
+    def __init__(self, production_rules, production_weights):
+        if len(production_weights) != len(production_rules):
+            raise ValueError("# of production rules and weights must match.")
+
+        self.production_rules = production_rules
+        if len(self.production_rules) > 0:
+            self.production_dist = dist.Categorical(production_weights)
+        else:
+            self.production_dist = None
+
+        self.attributes = []
+
+    def sample_production_rule(self, site_name, obs=None):
+        if not self.production_dist:
+            raise ValueError("Trying to sample from a terminal node.")
+        sampled_rule = pyro.sample(site_name, self.production_dist, obs=obs)
+        return self.production_rules[sampled_rule](*self.attributes)
+
+class TerminalNode(Node):
+    def __init__(self):
+        Node.__init__(self, [], [])
+
+class PlaceSetting(Node):
+    def __init__(self):
+        production_rules = [ AndRule(Dish), AndRule(Dish, Cup) ]
+        production_weights = torch.tensor([0.2, 0.8])
+        Node.__init__(self, production_rules, production_weights)
+
+class Dish(TerminalNode):
+    pass
+
+class Cup(TerminalNode):
+    pass
+
+class RootNode(Node):
+    def __init__(self):
+        production_rules = [ AndRule(PlaceSetting), AndRule(PlaceSetting, PlaceSetting) ]
+        production_weights = torch.tensor([ 0.75, 0.25 ])
+        Node.__init__(self, production_rules, production_weights)
+
 class ProbabilisticSceneGrammarModel():
-    class Node(object):
-        successors = []
-        def get_successors():
-            return self.successors
 
-    class TerminalNode(object):
-        pass
-
-    class RootNode(object):
-        pass
 
     def __init__(self):
+        pass
 
     def model(self, data=None):
-        nodes = [self._make_root_node()]
-        for node in nodes:
-
+        nodes = [RootNode()]
+        num_productions = 0
+        iter_k = 0
+        while len(nodes) > 0:
+            node = nodes.pop(0)
+            if isinstance(node, TerminalNode):
+                # Instantiate
+                print("Instantiating terminal node: ", node)
+            else:
+                print("Expanding non-terminal node ", node)
+                # Expand by picking a production rule
+                new_node_classes = node.sample_production_rule("rule_%04d" % num_productions)
+                [nodes.append(x) for x in new_node_classes]
+                num_productions += 1
+            iter_k += 1
 
         return pyro.sample("test", dist.Normal(0., 1.))
 
