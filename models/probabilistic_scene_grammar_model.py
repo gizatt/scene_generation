Bugfixes that were making log-likelihood grads wrt parameters come out incorrectly

models/probabilistic_scene_grammar_model.py
@@ -180,7 +180,7 @@ def score_tree(parse_tree, assert_rooted=True):
     by comparing a pyro-log-prob-sum of a forward run of the model
     to the value I compute here. Can I better use Pyro's machinery
     to do less work?'''
-    total_ll = torch.tensor(0.)
+    all_scores = []
     scores_by_node = {}
 
     for node in parse_tree.nodes:
@@ -201,9 +201,9 @@ def score_tree(parse_tree, assert_rooted=True):
         else:
             raise ValueError("Invalid node type in tree: ", type(node))
         scores_by_node[node] = node_score
-        total_ll = total_ll + node_score
+        all_scores.append(node_score)
 
-    return total_ll, scores_by_node
+    return torch.stack(all_scores).sum(), scores_by_node
 
 def score_terminal_node_productions(parse_tree):
     total_score = torch.tensor(0.)
@@ -415,7 +415,7 @@ def optimize_parse_tree_hmc_in_place(parse_tree, ax=None, verbose=False):
     num_dynamics_steps = 10
     epsilon_v = 5E-3
     epsilon_p = 5E-3
-    proposal_variance = 1.0
+    proposal_variance = 0.1
     for node in parse_tree:
         if isinstance(node, PlaceSetting):
             node.pose.requires_grad = True
@@ -526,15 +526,14 @@ def prune_node_from_tree(parse_tree, victim_node):
 
 
 def guess_parse_tree_from_yaml(yaml_env, outer_iterations=10, num_attempts=1, ax=None, verbose=False):
-    # Build an initial parse tree.
-    parse_tree = nx.DiGraph()
-    parse_tree.add_node(Table()) # Root node
-    for terminal_node in terminal_nodes_from_yaml(yaml_env):
-        parse_tree.add_node(terminal_node)
-
     best_tree = None
     best_score = -np.inf
     for attempt in range(num_attempts):
+        # Build an initial parse tree.
+        parse_tree = nx.DiGraph()
+        parse_tree.add_node(Table()) # Root node
+        for terminal_node in terminal_nodes_from_yaml(yaml_env):
+            parse_tree.add_node(terminal_node)
         for outer_k in range(outer_iterations):
             original_parse_tree_state = ParseTreeState(parse_tree)
             score, scores_by_node = score_tree(parse_tree)
@@ -611,7 +610,7 @@ def guess_parse_trees_batch(yaml_envs, outer_iterations, num_attempts):
     return all_observed_trees
 
 if __name__ == "__main__":
-    seed = 47
+    seed = 50
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
@@ -622,7 +621,7 @@ if __name__ == "__main__":
             
 
     plt.figure().set_size_inches(15, 10)
-    for k in range(100):
+    for k in range(1):
         start = time.time()
         pyro.clear_param_store()
         trace = poutine.trace(generate_unconditioned_parse_tree).get_trace()
@@ -649,8 +648,8 @@ if __name__ == "__main__":
         print("Trace score: %f" % trace.log_prob_sum())
         assert(abs(score - trace.log_prob_sum()) < 0.001)
 
-        with open("table_setting_environments_generated.yaml", "a") as file:
-            yaml.dump({"env_%d" % int(round(time.time() * 1000)): yaml_env}, file, Dumper=noalias_dumper)
+        #with open("table_setting_environments_generated.yaml", "a") as file:
+        #    yaml.dump({"env_%d" % int(round(time.time() * 1000)): yaml_env}, file, Dumper=noalias_dumper)
 
         #yaml_env = ProjectEnvironmentToFeasibility(yaml_env, base_environment_type="table_setting",
         #                                           make_static=False)[0]
@@ -671,22 +670,23 @@ if __name__ == "__main__":
         except:
             print(bcolors.FAIL, "Caught ????, probably sim fault due to weird geometry.", bcolors.ENDC)
 
-        #plt.figure()
-        #for k in range(3):
-        #    # And then try to parse it
-        #    ax = plt.subplot(3, 1, k+1)
-        #    plt.xlim(-0.2, 1.2)
-        #    plt.ylim(-0.2, 1.2)
+        plt.show()
+        plt.figure()
+        for k in range(3):
+            # And then try to parse it
+            ax = plt.subplot(3, 1, k+1)
+            plt.xlim(-0.2, 1.2)
+            plt.ylim(-0.2, 1.2)
 #
-        #    guessed_parse_tree, score = guess_parse_tree_from_yaml(yaml_env, outer_iterations=5, ax=plt.gca())
+            guessed_parse_tree, score = guess_parse_tree_from_yaml(yaml_env, outer_iterations=5, ax=plt.gca())
 #
-        #    print(guessed_parse_tree.nodes, guessed_parse_tree.edges)
-        #    plt.title("Guessed parse tree with score %f" % score)
-        #    plt.gca().clear()
-        #    draw_parse_tree(guessed_parse_tree)
-        #    plt.pause(1E-3)
+            print(guessed_parse_tree.nodes, guessed_parse_tree.edges)
+            plt.title("Guessed parse tree with score %f" % score)
+            plt.gca().clear()
+            draw_parse_tree(guessed_parse_tree)
+            plt.pause(1E-3)
 #
-        #plt.show()
+        plt.show()
         plt.pause(0.1)
         #trace = poutine.trace(model.model(observed_tree=guessed_parse_tree)).get_trace()
         #print("Trace log prob sum: %f" % trace.log_prob_sum())

models/probabilistic_scene_grammar_nodes.py
@@ -250,9 +250,9 @@ class PlaceSetting(CovaryingSetNode):
             self.object_type = object_type
             mean = pyro.param("place_setting_%s_mean" % object_name,
                               torch.tensor(mean_init))
-            var = pyro.param("place_setting_%s_var" % object_name,
+            var =  pyro.param("place_setting_%s_var" % object_name,
                               torch.tensor(var_init),
-                              constraint=constraints.positive)
+                              constraint=constraints.greater_than(0.001))
             self.param_names = ["place_setting_%s_mean" % object_name,
                                 "place_setting_%s_var" % object_name]
             self.offset_dist = dist.Normal(
@@ -270,7 +270,7 @@ class PlaceSetting(CovaryingSetNode):
                                        self.offset_dist, obs=obs_rel_pose)
                 return obs_products
             else:
-                rel_pose = pyro.sample("%s_pose" % (self.name), self.offset_dist)
+                rel_pose = pyro.sample("%s_pose" % (self.name), self.offset_dist).detach()
                 abs_pose = chain_pose_transforms(parent.pose, rel_pose)
                 return [self.object_type(name="%s_%s" % (self.name, self.object_name), pose=abs_pose)]
 
@@ -278,8 +278,17 @@ class PlaceSetting(CovaryingSetNode):
             if len(products) != 1 or not isinstance(products[0], self.object_type):
                 return torch.tensor(-np.inf)
             # Get relative offset of the PlaceSetting
-            rel_pose = self._recover_rel_pose_from_abs_pose(parent, products[0].pose)
-            return self.offset_dist.log_prob(rel_pose).sum()
+            rel_pose = self._recover_rel_pose_from_abs_pose(parent, products[0].pose.detach())
+            #if self.object_name == "left_fork":
+                #print("rel pose: ", rel_pose)
+                #print("parent pose: ", parent.pose)
+                #mean = pyro.param("place_setting_%s_mean" % self.object_name)
+                #print("mean: ", mean)
+                #score = ((products[0].pose - mean).sum())
+                #print("Log prob: ", score)
+                #score.backward(retain_graph=True)
+                #print("Grad of mean: ", mean.grad)
+            return self.offset_dist.log_prob(rel_pose)
 
     def __init__(self, name, pose):
         self.pose = pose
@@ -354,12 +363,14 @@ class Table(IndependentSetNode, RootNode):
                 products=[PlaceSetting])
             # Relative offset from root pose is drawn from a diagonal
             # Normal. It's rotated into the root pose frame at sample time.
-            mean = pyro.param("table_place_setting_mean",
-                              torch.tensor([0.0, 0., np.pi/2.]))
-            var = pyro.param("table_place_setting_var",
-                              torch.tensor([0.01, 0.01, 0.1]),
-                              constraint=constraints.positive)
-            self.param_names = ["table_place_setting_mean", "table_place_setting_var"]
+            #mean = pyro.param("table_place_setting_mean",
+            #                  torch.tensor([0.0, 0., np.pi/2.]))
+            #var = pyro.param("table_place_setting_var",
+            #                  torch.tensor([0.01, 0.01, 0.1]),
+            #                  constraint=constraints.positive)
+            #self.param_names = ["table_place_setting_mean", "table_place_setting_var"]
+            mean = torch.tensor([0.0, 0., np.pi/2.])
+            var = torch.tensor([0.01, 0.01, 0.1])
             self.offset_dist = dist.Normal(mean, var).to_event(1)
             self.pose = pose
 
@@ -376,7 +387,7 @@ class Table(IndependentSetNode, RootNode):
                 return obs_products
             else:
                 rel_offset = pyro.sample("%s_place_setting_offset" % self.name,
-                                         self.offset_dist)
+                                         self.offset_dist).detach()
                 # Rotate offset
                 pose_in_world = chain_pose_transforms(parent.pose, self.pose)
                 abs_offset = chain_pose_transforms(pose_in_world, rel_offset)
@@ -391,7 +402,7 @@ class Table(IndependentSetNode, RootNode):
 
     def __init__(self, name="table", num_place_setting_locations=4):
         self.pose = torch.tensor([0.5, 0.5, 0.])
-        self.table_radius = pyro.param("%s_radius" % name, torch.tensor(0.45), constraint=constraints.positive)
+        self.table_radius = 0.45 #pyro.param("%s_radius" % name, torch.tensor(0.45), constraint=constraints.positive)
         # Set-valued: a plate may appear at each location.
         production_rules = []
         for k in range(num_place_setting_locations):
@@ -406,8 +417,8 @@ class Table(IndependentSetNode, RootNode):
                 name="%s_prod_%03d" % (name, k), pose=pose))
         production_probs = pyro.param("%s_independent_set_production_probs" % name,
                                       torch.ones(num_place_setting_locations)*0.5,
-                                      constraint=constraints.greater_than_eq(0.))
-        self.param_names = ["%s_radius" % name,
+                                      constraint=constraints.unit_interval)
+        self.param_names = [#"%s_radius" % name,
                             "%s_independent_set_production_probs" % name]
         IndependentSetNode.__init__(self, name, production_rules, production_probs)
 