1922f51d17bc621a2445b7827c33c990efc14c18
Gregory Izatt <gregory.izatt@gmail.com>
2019-12-17 16:34:32 -0500

    Tweaking for proper output format and more controllability

--- data/generate_cardboard_box_environments.py
+++ data/generate_cardboard_box_environments.py
@@ -83,7 +83,7 @@ class RgbAndDepthAndLabelImageVisualizer(LeafSystem):
     def __init__(self,
                  depth_camera_properties,
                  draw_timestep=0.1,
-                 out_dir=None):
+                 out_prefix=None):
         LeafSystem.__init__(self)
         self.set_name('image viz')
         self.timestep = draw_timestep
@@ -100,7 +100,7 @@ class RgbAndDepthAndLabelImageVisualizer(LeafSystem):
                                    AbstractValue.Make(Image[PixelType.kLabel16I](640, 480, 1)))
         self.fig = plt.figure()
         self.ax = plt.gca()
-        self.out_dir = out_dir
+        self.out_prefix = out_prefix
         self.iter_count = 0
         self.depth_near = depth_camera_properties.z_near
         self.depth_far = depth_camera_properties.z_far
@@ -117,35 +117,51 @@ class RgbAndDepthAndLabelImageVisualizer(LeafSystem):
 
         print("Shapes: ", rgb_image.shape, depth_image.shape, label_image.shape)
         #rgb_image = np.frombuffer(rgb_image.data, dtype=np.uint8).reshape(rgb_image.shape, order='C')
-        PIL.Image.fromarray(rgb_image, mode='RGBA').save(os.path.join(self.out_dir, "00_%08d_drake_col.png" % self.iter_count))
+        PIL.Image.fromarray(rgb_image, mode='RGBA').save(self.out_prefix + "%08d_drake_col.png" % self.iter_count)
 
         #depth_image = np.frombuffer(depth_image.data, dtype=np.uint16).reshape(depth_image.shape, order='C').astype(np.int32)
         depth_image = ((depth_image.astype(float) / 2**16) * (self.depth_far - self.depth_near) + self.depth_near)*1000
         depth_image = depth_image.astype(np.int32)
-        PIL.Image.fromarray(depth_image[:, :, 0], mode='I').save(os.path.join(self.out_dir, "00_%08d_drake_depth.png" % self.iter_count))
+        PIL.Image.fromarray(depth_image[:, :, 0], mode='I').save(self.out_prefix + "%08d_drake_depth.png" % self.iter_count)
 
         #label_image = np.frombuffer(label_image.data, dtype=np.int16).reshape(label_image.shape, order='C').astype(np.int32)
-        print("Min and max labels: ", np.min(label_image), np.max(label_image))
-        PIL.Image.fromarray(label_image[:, :, 0].astype(np.int32), mode='I').save(os.path.join(self.out_dir, "00_%08d_drake_label.png" % self.iter_count))
+        PIL.Image.fromarray(label_image[:, :, 0].astype(np.int32), mode='I').save(self.out_prefix + "%08d_drake_label.png" % self.iter_count)
         colored_label_image = (colorize_labels(label_image)*255).astype(np.int8)
-        print("Colored label image size: ", colored_label_image.shape)
-        PIL.Image.fromarray(colored_label_image, mode='RGB').save(os.path.join(self.out_dir, "00_%08d_drake_label_colored.png" % self.iter_count))
+        PIL.Image.fromarray(colored_label_image, mode='RGB').save(self.out_prefix + "%08d_drake_label_colored.png" % self.iter_count)
 
+        print("Saved to ", self.out_prefix + "%08d_drake_label_colored.png" % self.iter_count)
         self.iter_count += 1
 
 
 if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_scenes", type=int, help="Number of scenes to generate.")
+    parser.add_argument("-i", "--cardboard_boxes_dir", type=str,
+                        help="Directory containing box models.",
+                        default="cardboard_boxes")
+    parser.add_argument("-n", "--num_cameras", type=int,
+                        help="Number of simultaneous camera views.",
+                        default=3)
+    parser.add_argument("-o", "--output_dir", type=str,
+                        help="Directory to output scenes into.",
+                        default="generated_scenes")
+    parser.add_argument("-t", "--sim_time", type=float,
+                        help="Duration to simulate.",
+                        default=1.0)
+    args = parser.parse_args()
 
+    out_dir = args.output_dir
+    os.system("mkdir -p %s" % out_dir)
 
-    d = "cardboard_boxes"
+    assert(os.path.exists(args.cardboard_boxes_dir))
+    d = args.cardboard_boxes_dir
     candidate_model_files = [
         os.path.abspath(os.path.join(d, o, "box.sdf")) for o in os.listdir(d) 
         if os.path.isdir(os.path.join(d ,o))
     ]
 
-    #np.random.seed(42)
-    #random.seed(42)
-    for scene_iter in range(1):
+    for scene_iter in range(args.num_scenes):
         try:
             builder = DiagramBuilder()
             mbp, scene_graph = AddMultibodyPlantSceneGraph(
@@ -172,18 +188,26 @@ if __name__ == "__main__":
             n_objects = np.random.randint(3, 10)
             poses = []  # [quat, pos]
             classes = []
+            label_indices = []
             material_overrides = []
             for k in range(n_objects):
                 model_name = "model_%d" % k
                 model_ind = np.random.randint(0, len(candidate_model_files))
                 class_path = candidate_model_files[model_ind]
                 classes.append(class_path)
-                parser.AddModelFromFile(class_path, model_name=model_name)
+                model_index = parser.AddModelFromFile(class_path, model_name=model_name)
                 poses.append([
                     RollPitchYaw(np.random.uniform(0., 2.*np.pi, size=3)).ToQuaternion().wxyz(),
                     [np.random.uniform(-0.1, 0.1),
                      np.random.uniform(-0.1, 0.1),
                      np.random.uniform(0.1, 0.2)]])
+                # Get body indices for this model, which drive the labels that'll be
+                # assigned to this model
+                possible_label_ids = mbp.GetBodyIndices(model_index)
+                if len(possible_label_ids) != 1:
+                    raise NotImplementedError("Parsed model had more than one body -- this'll"
+                                              " mess up the label image.")
+                label_indices.append(int(possible_label_ids[0]))
                 material_overrides.append(
                     (".*model_%d.*" % k,
                      {"material_type": "CC0_texture",
@@ -197,86 +221,7 @@ if __name__ == "__main__":
             #builder.Connect(scene_graph.get_pose_bundle_output_port(),
             #                visualizer.get_input_port(0))
 
-            ## BLENDER SERVER STUFF
-            cam_trans_base = np.array([0., 0., np.random.uniform(0.7, 1.5)])
-            # Rotate randomly around z axis
-            cam_quat_base = RollPitchYaw(
-                0., 0., np.random.uniform(0., np.pi*2.)).ToQuaternion()
-            cam_tf_base = Isometry3(quaternion=cam_quat_base,
-                                    translation=cam_trans_base)
-            # Rotate that camera away from vertical by up to pi/2
-            cam_tf_base = Isometry3(quaternion=RollPitchYaw(np.random.uniform(-np.pi/2, np.pi/2), 0., 0.).ToQuaternion(),
-                                    translation=[0, 0, 0]).multiply(cam_tf_base)
-            # And then rotate again around z axis
-            cam_tf_base = Isometry3(quaternion=RollPitchYaw(0., 0., np.random.uniform(-np.pi/2, np.pi/2)).ToQuaternion(),
-                                    translation=[0, 0, 0]).multiply(cam_tf_base)
-            cam_tfs = [cam_tf_base]
-
-            offset_quat_base = RollPitchYaw(0., 0., 0.).ToQuaternion().wxyz()
-            out_dir = "/home/gizatt/data/generated_cardboard_envs/scene_%03d/" % scene_iter
-            os.system("mkdir -p %s" % out_dir)
-
-            # Add materials for ground
-            material_overrides.append(
-                (".*ground.*",
-                 {"material_type": "CC0_texture",
-                  "path": 
-                    random.choice(["/home/gizatt/tools/blender_server/data/test_pbr_mats/Wood15/Wood15",
-                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Wood08/Wood08",
-                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Metal09/Metal09",
-                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Metal26/Metal26"])}))
-            blender_color_cam = builder.AddSystem(BlenderColorCamera(
-                scene_graph,
-                draw_period=1.0,
-                camera_tfs=cam_tfs,
-                zmq_url="tcp://127.0.0.1:5556",
-                env_map_path=random.choice(
-                    ["/home/gizatt/tools/blender_server/data/env_maps/aerodynamics_workshop_4k.hdr",
-                     "/home/gizatt/tools/blender_server/data/env_maps/cave_wall_4k.hdr",
-                     "/home/gizatt/tools/blender_server/data/env_maps/lab_from_phone.jpg",
-                     "/home/gizatt/tools/blender_server/data/env_maps/small_hangar_01_4k.hdr"]),
-                material_overrides=material_overrides,
-                global_transform=Isometry3(translation=[0, 0, 0],
-                                           quaternion=Quaternion(offset_quat_base)),
-                out_prefix=out_dir
-            ))
-            builder.Connect(scene_graph.get_pose_bundle_output_port(),
-                            blender_color_cam.get_input_port(0))
-
-            # Add equivalent camera on Drake side
-            
-            # Rotate cam to get it from blender +y up, +x right, -z forward
-            # to Drake X-right, Y-Down, Z-Forward
-            new_rot = cam_tf_base.matrix()[:3, :3].dot(RollPitchYaw([np.pi, 0., 0.]).ToRotationMatrix().matrix())
-            new_cam_tf = RigidTransform(R=RotationMatrix(new_rot), p=cam_tf_base.translation())
-            print("New cam tf: ", new_cam_tf.rotation().matrix(), new_cam_tf.translation())
-            
-            # Blender is using 90* FOV along the long dimension (width)
-            # Find equivalent aspect ratio along vertical dim (y)
-            hfov = np.pi/2.
-            width = 640
-            height = 480
-            f = 0.5 * width / np.tan(hfov/2)
-            vfov = 2 * np.arctan2( .5 * height, f)
-            print("Vfov: ", vfov)
-            depth_camera_properties = DepthCameraProperties(
-                width=640, height=480, fov_y=vfov, renderer_name="renderer", z_near=0.1, z_far=3.0)
-            parent_frame_id = scene_graph.world_frame_id()
-            # Above origin facing straight down
-            camera = builder.AddSystem(
-                RgbdSensor(parent_frame_id, new_cam_tf, depth_camera_properties, show_window=False))
-            builder.Connect(scene_graph.get_query_output_port(),
-                            camera.query_object_input_port())
-
-            camera_viz = builder.AddSystem(RgbAndDepthAndLabelImageVisualizer(
-                depth_camera_properties=depth_camera_properties, 
-                draw_timestep=1.0, out_dir=out_dir))
-            builder.Connect(camera.color_image_output_port(),
-                            camera_viz.get_input_port(0))
-            builder.Connect(camera.depth_image_16U_output_port(),
-                            camera_viz.get_input_port(1))
-            builder.Connect(camera.label_image_output_port(),
-                            camera_viz.get_input_port(2))
+            # Simulate to stability and get statically stable pose.
             diagram = builder.Build()
 
             diagram_context = diagram.CreateDefaultContext()
@@ -290,8 +235,6 @@ if __name__ == "__main__":
                 offset = k*7
                 q0[(offset):(offset+4)] = poses[k][0]
                 q0[(offset+4):(offset+7)] = poses[k][1]
-
-
 
             simulator = Simulator(diagram, diagram_context)
             simulator.set_target_realtime_rate(1.0)
@@ -349,26 +292,141 @@ if __name__ == "__main__":
             q0_proj = result.GetSolution(q_dec)
             mbp.SetPositions(mbp_context, q0_proj)
             q0_initial = q0_proj.copy()
-            simulator.StepTo(1.1)
+            simulator.StepTo(args.sim_time + 0.1)
             q0_final = mbp.GetPositions(mbp_context).copy()
 
-            output_dict = {"n_objects": len(poses)}
+            ## RENDERING STUFF
+            rendering_builder = DiagramBuilder()
+            cam_tfs = []
+            for camera_k in range(args.num_cameras):
+                cam_trans_base = np.array([0., 0., np.random.uniform(0.7, 1.5)])
+                # Rotate randomly around z axis
+                cam_quat_base = RollPitchYaw(
+                    0., 0., np.random.uniform(0., np.pi*2.)).ToQuaternion()
+                cam_tf_base = Isometry3(quaternion=cam_quat_base,
+                                        translation=cam_trans_base)
+                # Rotate that camera away from vertical by up to pi/2
+                cam_tf_base = Isometry3(quaternion=RollPitchYaw(np.random.uniform(-np.pi/2, np.pi/2), 0., 0.).ToQuaternion(),
+                                        translation=[0, 0, 0]).multiply(cam_tf_base)
+                # And then rotate again around z axis
+                cam_tf_base = Isometry3(quaternion=RollPitchYaw(0., 0., np.random.uniform(-np.pi/2, np.pi/2)).ToQuaternion(),
+                                        translation=[0, 0, 0]).multiply(cam_tf_base)
+                cam_tfs.append(cam_tf_base)
+
+            offset_quat_base = RollPitchYaw(0., 0., 0.).ToQuaternion().wxyz()
+            out_dir_iter = os.path.abspath(os.path.join(out_dir, "scene_%03d" % scene_iter)) + '/'
+            os.system("mkdir -p %s" % out_dir_iter)
+
+            # Add materials for ground
+            material_overrides.append(
+                (".*ground.*",
+                 {"material_type": "CC0_texture",
+                  "path": 
+                    random.choice(["/home/gizatt/tools/blender_server/data/test_pbr_mats/Wood15/Wood15",
+                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Wood08/Wood08",
+                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Metal09/Metal09",
+                                   "/home/gizatt/tools/blender_server/data/test_pbr_mats/Metal26/Metal26"])}))
+            blender_color_cam = BlenderColorCamera(
+                scene_graph,
+                draw_period=args.sim_time,
+                camera_tfs=cam_tfs,
+                zmq_url="tcp://127.0.0.1:5556",
+                env_map_path=random.choice(
+                    ["/home/gizatt/tools/blender_server/data/env_maps/aerodynamics_workshop_4k.hdr",
+                     "/home/gizatt/tools/blender_server/data/env_maps/cave_wall_4k.hdr",
+                     "/home/gizatt/tools/blender_server/data/env_maps/lab_from_phone.jpg",
+                     "/home/gizatt/tools/blender_server/data/env_maps/small_hangar_01_4k.hdr"]),
+                material_overrides=material_overrides,
+                global_transform=Isometry3(translation=[0, 0, 0],
+                                           quaternion=Quaternion(offset_quat_base)),
+                out_prefix=out_dir_iter
+            )
+            cam_context = blender_color_cam.CreateDefaultContext()
+            cam_context.SetTime(args.sim_time)
+            cam_context.FixInputPort(
+                blender_color_cam.get_input_port(0).get_index(),
+                AbstractValue.Make(
+                    scene_graph.get_pose_bundle_output_port().Eval(sg_context)))
+            blender_color_cam.load() # How do I do this through drake systems?
+            blender_color_cam.Publish(cam_context)
+
+            print("Starting Drake camera renders")
+
+            # Add and render equivalent cameras on Drake side
+            for camera_k in range(args.num_cameras):
+                rendering_builder = DiagramBuilder()
+                # Rotate cam to get it from blender +y up, +x right, -z forward
+                # to Drake X-right, Y-Down, Z-Forward
+                cam_tf_base = cam_tfs[camera_k]
+                new_rot = cam_tf_base.matrix()[:3, :3].dot(RollPitchYaw([np.pi, 0., 0.]).ToRotationMatrix().matrix())
+                new_cam_tf = RigidTransform(R=RotationMatrix(new_rot), p=cam_tf_base.translation())
+
+                # Blender is using 90* FOV along the long dimension (width)
+                # Find equivalent aspect ratio along vertical dim (y)
+                hfov = np.pi/2.
+                width = 640
+                height = 480
+                f = 0.5 * width / np.tan(hfov/2)
+                vfov = 2 * np.arctan2( .5 * height, f)
+                depth_camera_properties = DepthCameraProperties(
+                    width=640, height=480, fov_y=vfov, renderer_name="renderer", z_near=0.1, z_far=3.0)
+                parent_frame_id = scene_graph.world_frame_id()
+                # Above origin facing straight down
+                camera = rendering_builder.AddSystem(
+                    RgbdSensor(parent_frame_id, new_cam_tf, depth_camera_properties, show_window=False))
+                rendering_builder.ExportInput(camera.query_object_input_port(), "camera_query_object_input")
+
+                camera_viz = rendering_builder.AddSystem(RgbAndDepthAndLabelImageVisualizer(
+                    depth_camera_properties=depth_camera_properties, 
+                    draw_timestep=args.sim_time, out_prefix=out_dir_iter + "/%02d_" % camera_k))
+                rendering_builder.Connect(camera.color_image_output_port(),
+                                          camera_viz.get_input_port(0))
+                rendering_builder.Connect(camera.depth_image_16U_output_port(),
+                                          camera_viz.get_input_port(1))
+                rendering_builder.Connect(camera.label_image_output_port(),
+                                          camera_viz.get_input_port(2))
+
+                rendering_diagram = rendering_builder.Build()
+                rendering_diagram_context = rendering_diagram.CreateDefaultContext()
+                rendering_diagram_context.SetTime(args.sim_time)
+                rendering_diagram_context.FixInputPort(
+                    camera.query_object_input_port().get_index(),
+                    AbstractValue.Make(
+                        scene_graph.get_query_output_port().Eval(sg_context))
+                )
+                rendering_diagram.Publish(rendering_diagram_context)
+
+            cam_info_list = []
+            for camera_k in range(args.num_cameras):
+                pose = np.hstack([cam_tfs[camera_k].quaternion().wxyz(),
+                                  cam_tfs[camera_k].translation()]).tolist()
+                camera_info_dict = {
+                    "pose": pose,
+                    "rgb_image_filename": "%02d_%08d.jpg" % (camera_k, 0),
+                    "label_image_filename": "%02d_%08d_drake_label.png" % (camera_k, 0),
+                    "depth_image_filename": "%02d_%08d_drake_depth.png" % (camera_k, 0),
+                }
+                cam_info_list.append(camera_info_dict)
+
+            output_dict = {
+                "n_objects": len(poses),
+                "cameras": cam_info_list
+            }
             for k in range(len(poses)):
                 offset = k*7
                 pose = q0[(offset):(offset+7)]
                 output_dict["obj_%04d" % k] = {
-                    "class": classes[k],
-                    "pose": pose.tolist()
+                    "class": "prime_box",
+                    "sdf": classes[k],
+                    "pose": pose.tolist(),
+                    "label_index": label_indices[k]
                 }
-            with open(os.path.join(out_dir, "object_poses.yaml"), "w") as file:
+            with open(os.path.join(out_dir_iter, "scene_info.yaml"), "a") as file:
                 yaml.dump({"env_%d" % int(round(time.time() * 1000)):
                            output_dict},
                           file)
-#
-            time.sleep(1)
 
         except Exception as e:
             print("Unhandled exception ", e)
-
         except:
             print("Unhandled unnamed exception, probably sim error")
