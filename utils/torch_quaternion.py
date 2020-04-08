5f9cf94b2bc4a0eb2165d5eaa79cf4aba18eea2c
Greg Izatt <gizatt@mit.edu>
2020-04-07 20:06:43 -0400

    Add some rotmat conversion routines

--- utils/torch_quaternion.py
+++ utils/torch_quaternion.py
@@ -194,7 +194,114 @@ def euler_to_quaternion(e, order):
         result *= -1
     
     return result.reshape(original_shape)
-    
+
+def quat2mat(quat):
+    """
+    From https://github.com/ClementPinard/SfmLearner-Pytorch/blob/master/inverse_warp.py
+    Convert quaternion coefficients to rotation matrix.
+    Args:
+        quat: first three coeff of quaternion of rotation. fourht is then computed to have a norm of 1 -- size = [B, 3]
+    Returns:
+        Rotation matrix corresponding to the quaternion -- size = [B, 3, 3]
+    """
+    norm_quat = torch.cat([quat[:,:1].detach()*0 + 1, quat], dim=1)
+    norm_quat = norm_quat/norm_quat.norm(p=2, dim=1, keepdim=True)
+    w, x, y, z = norm_quat[:,0], norm_quat[:,1], norm_quat[:,2], norm_quat[:,3]
+
+    B = quat.size(0)
+
+    w2, x2, y2, z2 = w.pow(2), x.pow(2), y.pow(2), z.pow(2)
+    wx, wy, wz = w*x, w*y, w*z
+    xy, xz, yz = x*y, x*z, y*z
+
+    rotMat = torch.stack([w2 + x2 - y2 - z2, 2*xy - 2*wz, 2*wy + 2*xz,
+                          2*wz + 2*xy, w2 - x2 + y2 - z2, 2*yz - 2*wx,
+                          2*xz - 2*wy, 2*wx + 2*yz, w2 - x2 - y2 + z2], dim=1).reshape(B, 3, 3)
+    return rotMat
+
+def rotation_matrix_to_quaternion(
+        rotation_matrix: torch.Tensor,
+        eps: float = 1e-8) -> torch.Tensor:
+    r"""
+
+    From https://github.com/kornia/kornia/blob/master/kornia/geometry/conversions.py
+
+    Convert 3x3 rotation matrix to 4d quaternion vector.
+    The quaternion vector has components in (x, y, z, w) format.
+    Args:
+        rotation_matrix (torch.Tensor): the rotation matrix to convert.
+        eps (float): small value to avoid zero division. Default: 1e-8.
+    Return:
+        torch.Tensor: the rotation in quaternion.
+    Shape:
+        - Input: :math:`(*, 3, 3)`
+        - Output: :math:`(*, 4)`
+    Example:
+        >>> input = torch.rand(4, 3, 3)  # Nx3x3
+        >>> output = kornia.rotation_matrix_to_quaternion(input)  # Nx4
+    """
+    if not isinstance(rotation_matrix, torch.Tensor):
+        raise TypeError("Input type is not a torch.Tensor. Got {}".format(
+            type(rotation_matrix)))
+
+    if not rotation_matrix.shape[-2:] == (3, 3):
+        raise ValueError(
+            "Input size must be a (*, 3, 3) tensor. Got {}".format(
+                rotation_matrix.shape))
+
+    def safe_zero_division(numerator: torch.Tensor,
+                           denominator: torch.Tensor) -> torch.Tensor:
+        eps: float = torch.finfo(numerator.dtype).tiny  # type: ignore
+        return numerator / torch.clamp(denominator, min=eps)
+
+    rotation_matrix_vec: torch.Tensor = rotation_matrix.reshape(
+        *rotation_matrix.shape[:-2], 9)
+
+    m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.chunk(
+        rotation_matrix_vec, chunks=9, dim=-1)
+
+    trace: torch.Tensor = m00 + m11 + m22
+
+    def trace_positive_cond():
+        sq = torch.sqrt(trace + 1.0) * 2.  # sq = 4 * qw.
+        qw = 0.25 * sq
+        qx = safe_zero_division(m21 - m12, sq)
+        qy = safe_zero_division(m02 - m20, sq)
+        qz = safe_zero_division(m10 - m01, sq)
+        return torch.cat([qx, qy, qz, qw], dim=-1)
+
+    def cond_1():
+        sq = torch.sqrt(1.0 + m00 - m11 - m22 + eps) * 2.  # sq = 4 * qx.
+        qw = safe_zero_division(m21 - m12, sq)
+        qx = 0.25 * sq
+        qy = safe_zero_division(m01 + m10, sq)
+        qz = safe_zero_division(m02 + m20, sq)
+        return torch.cat([qx, qy, qz, qw], dim=-1)
+
+    def cond_2():
+        sq = torch.sqrt(1.0 + m11 - m00 - m22 + eps) * 2.  # sq = 4 * qy.
+        qw = safe_zero_division(m02 - m20, sq)
+        qx = safe_zero_division(m01 + m10, sq)
+        qy = 0.25 * sq
+        qz = safe_zero_division(m12 + m21, sq)
+        return torch.cat([qx, qy, qz, qw], dim=-1)
+
+    def cond_3():
+        sq = torch.sqrt(1.0 + m22 - m00 - m11 + eps) * 2.  # sq = 4 * qz.
+        qw = safe_zero_division(m10 - m01, sq)
+        qx = safe_zero_division(m02 + m20, sq)
+        qy = safe_zero_division(m12 + m21, sq)
+        qz = 0.25 * sq
+        return torch.cat([qx, qy, qz, qw], dim=-1)
+
+    where_2 = torch.where(m11 > m22, cond_2(), cond_3())
+    where_1 = torch.where(
+        (m00 > m11) & (m00 > m22), cond_1(), where_2)
+
+    quaternion: torch.Tensor = torch.where(
+        trace > 0., trace_positive_cond(), where_1)
+    return quaternion
+
 if __name__ == "__main__":
     print("Starting tests to try to find correspondence with Drake conventions")
 
