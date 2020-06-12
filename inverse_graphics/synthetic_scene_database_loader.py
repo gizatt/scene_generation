6d6940ed1e2a91493c97849bc587575048e088b9
Greg Izatt <gizatt@mit.edu>
2020-06-12 19:04:13 -0400

    Undo the range change on rgb images

--- inverse_graphics/synthetic_scene_database_loader.py
+++ inverse_graphics/synthetic_scene_database_loader.py
@@ -354,7 +354,7 @@ class XenRCNNMapper:
         """
         #dataset_dict = {key: value for key, value in dataset_dict.items()}
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-        image = utils.read_image(dataset_dict["file_name"], format=self.img_format).astype(np.float32)/255.
+        image = utils.read_image(dataset_dict["file_name"], format=self.img_format).astype(np.float32)
         # convert depth to meters
         depth_image = utils.read_image(dataset_dict["depth_file_name"], format='I').astype(np.float32)/1000.
         utils.check_image_size(dataset_dict, image)
